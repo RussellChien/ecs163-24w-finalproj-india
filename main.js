98cb7f55cfd354fd52fd936b7c166aee51c096e9
Russell Chien <RussellChien6@gmail.com>
2024-03-10 23:37:06 -0700

    clean dataset + fix formatting + add parallel coordinates plot + start bar charts
    Co-authored-by: Orry Nicholas Butler <onbutler@ucdavis.edu>
    Co-authored-by: johnmema <johnmema@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,28 +1,52 @@
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = screenWidth - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+    width = screenWidth / 2 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-const options = ["Access to electricity (% of population)", "CO2 emissions (metric tons per capita)", "Individuals using the Internet (% of population)", "Population", "Renewable energy consumption (% of total final energy consumption)", "Exports of goods and services (% of GDP)", "Foreign direct investment net (BoP current US$)", "Foreign direct investment net inflows (% of GDP)", "Population growth (annual %)",
-"Central government debt total (% of GDP)", "GNI (current US$)", "Net trade in goods and services (BoP current US$)", "Unemployment total (% of total labor force) (modeled ILO estimate)", "School enrollment secondary (% gross)", "Computer communications and other services (% of commercial service imports)", "Urban population (% of total population)", "International tourism receipts (current US$)", "Agriculture forestry and fishing value added (constant 2015 US$)",
-"Adjusted net national income (constant 2015 US$)", "Industry (including construction) value added (constant 2015 US$)", "Manufacturing value added (current US$)", "Agricultural land (sq. km)"]
+const dimensions = [
+    "GDP (current US$)",
+    "Individuals using the Internet (% of population)",
+    "Exports of goods and services (% of GDP)",
+    "Industry (including construction) value added (constant 2015 US$)",
+    "Foreign direct investment net inflows (% of GDP)"
+]
+const dimensionLabels = {
+    'GDP (current US$)': 'GDP',
+    'Individuals using the Internet (% of population)': 'Internet Users',
+    'Exports of goods and services (% of GDP)': 'Exports',
+    'Industry (including construction) value added (constant 2015 US$)': 'Industry Value Added',
+    'Foreign direct investment net inflows (% of GDP)': 'FDI Inflows'
+}
+const options = [
+    "Individuals using the Internet (% of population)",
+    "Exports of goods and services (% of GDP)",
+    "Industry (including construction) value added (constant 2015 US$)",
+    "Foreign direct investment net inflows (% of GDP)"
+]
+const optionLabels = {
+    "Individuals using the Internet (% of population)": "Individuals using the Internet (% of population)",
+    "Exports of goods and services (% of GDP)": "Exports of goods and services (% of GDP)",
+    "Industry (including construction) value added (constant 2015 US$)": "Industry value added (constant 2015 US$)",
+    "Foreign direct investment net inflows (% of GDP)": "Foreign direct investment net inflows (% of GDP)"
+}
 
-d3.select('#selector')
-.selectAll("options")
-.data(options)
-.enter()
-.append('option')
-.text(function (d) {return d})
-.attr("value", function(d) {
-    return d
-})
+d3.select('#line_selector')
+    .selectAll("options")
+    .data(options)
+    .enter()
+    .append('option')
+    .text(function (d) { return d; })
+    .attr("value", function (d) {
+        return d
+    })
 
-d3.csv("data/india_dataset.csv").then(rawData =>{
+// line graphs
+d3.csv("data/india_dataset.csv").then(rawData => {
     rawData = rawData.map(d => {
         return {
-            "IndicatorName" : d.IndicatorName,
-            "IndicatorCode" : d.IndicatorCode,
+            "IndicatorName": d.IndicatorName,
+            "IndicatorCode": d.IndicatorCode,
             "1993": Number(d["1993"]),
             "1994": Number(d["1994"]),
             "1995": Number(d["1995"]),
@@ -54,133 +78,144 @@ d3.csv("data/india_dataset.csv").then(rawData =>{
             "2021": Number(d["2021"]),
             "2022": Number(d["2022"])
         }
-                        
+
     })
     console.log(rawData)
-    var svg = d3.select("svg")
+    var svg = d3.select("#line-graph")
+        .append("svg")
+        // const g1 = svg.append("g")
+        .attr("width", width + 60)
+        .attr("height", height + 60)
+        .attr("transform", `translate(${margin.left}, ${margin.top + 20})`)
 
-    const g1 = svg.append("g")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    // x axis
+    // g1
+    svg.append("text")
+        .classed("x_label", true)
+        .attr("x", width / 2)
+        .attr("y", height + 40)
+        .attr("font-size", "20px")
+        .attr("text-anchor", "middle")
+        .text("Individuals using the Internet (% of population)")
 
-    g1.append("text")
-    .classed("x_label", true)
-    .attr("x", width / 2)
-    .attr("y", height + 50)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .text("Access to electricity (% of population)")
+    // y axis
+    //g1
+    svg.append("text")
+        .attr("x", -(height / 2))
+        .attr("y", 15)
+        .attr("font-size", "20px")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .text("GDP (current US$)")
 
-    g1.append("text")
-    .attr("x", -(height / 2))
-    .attr("y", -40)
-    .attr("font-size", "20px")
-    .attr("text-anchor", "middle")
-    .attr("transform", "rotate(-90)")
-    .text("GDP (current US$)")
-
+    // x scale 
     let x1 = d3.scaleLinear()
-    .domain([d3.min(rawData, function(d) {
-        if (d.IndicatorCode == "NY.GDP.MKTP.CD") {
-            let min_value = Infinity
-            for (let i = 1993; i <= 2021; i++) {
-                if (d[i] < min_value) {
-                    min_value = d[i]
+        .domain([d3.min(rawData, function (d) {
+            if (d.IndicatorName == "Individuals using the Internet (% of population)") {
+                let min_value = Infinity
+                for (let i = 1993; i <= 2021; i++) {
+                    if (d[i] < min_value) {
+                        min_value = d[i]
+                    }
                 }
+                return min_value
             }
-        return min_value
-        }
-    }), d3.max(rawData, function(d) {
-        if (d.IndicatorCode == "NY.GDP.MKTP.CD") {
-            let max_value = 0
-            for (let i = 1993; i <= 2021; i++) {
-                if (d[i] > max_value) {
-                    max_value = d[i]
+        }), d3.max(rawData, function (d) {
+            if (d.IndicatorName == "Individuals using the Internet (% of population)") {
+                let max_value = 0
+                for (let i = 1993; i <= 2021; i++) {
+                    if (d[i] > max_value) {
+                        max_value = d[i]
+                    }
                 }
+                return max_value
             }
-        return max_value
-        }
-    })])
-    .range([0, width])
+        })])
+        .range([0, width])
 
     let xAxisCall = d3.axisBottom(x1)
-    .ticks(7)
-    g1.append("g")
-    .classed("x_axis", true)
-    .attr("transform", `translate(0, ${height})`)
-    .call(xAxisCall)
-    .selectAll("text")
-    .attr("y", "10")
-    .attr("x", "0")
-    .attr("text-anchor", "middle")
+        .ticks(7)
+    //g1
+    svg.append("g")
+        .classed("x_axis", true)
+        .attr("transform", `translate(${50}, ${height})`)
+        .call(xAxisCall)
+        .selectAll("text")
+        .attr("y", "10")
+        .attr("x", "0")
+        .attr("text-anchor", "middle")
 
+    // y scale
     let y1 = d3.scaleLinear()
-    .domain([d3.max(rawData, function(d) {
-        if (d.IndicatorCode == "EG.ELC.ACCS.ZS") {
-            let max_value = 0
-            for (let i = 1993; i <= 2021; i++) {
-                if (d[i] > max_value) {
-                    max_value = d[i]
+        .domain([d3.max(rawData, function (d) {
+            if (d.IndicatorCode == "EG.ELC.ACCS.ZS") {
+                let max_value = 0
+                for (let i = 1993; i <= 2020; i++) {
+                    if (d[i] > max_value) {
+                        max_value = d[i]
+                    }
                 }
+                return max_value
             }
-            return max_value
-        }
-    }), d3.min(rawData, function(d) {
-        if (d.IndicatorCode == "EG.ELC.ACCS.ZS") {
-            let min_value = Infinity
-            for (let i = 1993; i <= 2021; i++) {
-                if (d[i] < min_value) {
-                    min_value = d[i]
+        }), d3.min(rawData, function (d) {
+            if (d.IndicatorCode == "EG.ELC.ACCS.ZS") {
+                let min_value = Infinity
+                for (let i = 1993; i <= 2020; i++) {
+                    if (d[i] < min_value) {
+                        min_value = d[i]
+                    }
                 }
+                console.log(min_value)
+                return min_value
             }
-        console.log(min_value)
-        return min_value
-        }
-    })])
-    .range([0, height])
+        })])
+        .range([0, height])
 
     let yAxisCall = d3.axisLeft(y1)
-                        .ticks(13)
-    g1.append("g").call(yAxisCall)
+        .ticks(13)
+    //g1
+    svg.append("g").attr("transform", `translate(${50}, ${0})`).call(yAxisCall)
 
-    const filteredData =rawData.filter(d => d.IndicatorCode === "NY.GDP.MKTP.CD" || d.IndicatorCode === "EG.ELC.ACCS.ZS")
+    const filteredData = rawData.filter(d => d.IndicatorName === "Individuals using the Internet (% of population)" || d.IndicatorCode === "EG.ELC.ACCS.ZS")
 
     let grpah1dataPoints = []
 
-    for (let i = 1993; i <= 2021; i ++) {
-        let graph1point = [filteredData[0][i], filteredData[1][i]]
+    for (let i = 1993; i <= 2020; i++) {
+        let graph1point = [filteredData[1][i], filteredData[0][i]]
         grpah1dataPoints.push(graph1point)
     }
     console.log(grpah1dataPoints)
-        let graph_path = g1.append("path")
-        .datum(grpah1dataPoints)
-        .attr("fill", "none")
-        .attr("stroke", "#138808")
-        .attr("stroke-width", 1.5)
-        .attr("d", d3.line()
-        .x(function(d) {
-            return x1(d[0])
-        }).y(function(d) {
-            return y1(d[1])
-        }))
+    let graph_path = //g1
+        svg.append("path")
+            .datum(grpah1dataPoints)
+            .attr("transform", `translate(${50}, ${0})`)
+            .attr("fill", "none")
+            .attr("stroke", "#138808")
+            .attr("stroke-width", 1.5)
+            .attr("d", d3.line()
+                .x(function (d) {
+                    return x1(d[0])
+                }).y(function (d) {
+                    return y1(d[1])
+                }))
 
-        let length = graph_path.node().getTotalLength()
+    let length = graph_path.node().getTotalLength()
 
-        graph_path.attr("stroke-dasharray", length + " " + length)
+    graph_path.attr("stroke-dasharray", length + " " + length)
         .attr("stroke-dashoffset", length)
         .transition()
         .duration(5000)
         .attr("stroke-dashoffset", 0)
 
-    d3.select('#selector').on("change", function(d) {
+    // add the X Axis from selector
+    d3.select('#line_selector').on("change", function (d) {
         let new_option = d3.select(this).property("value")
         console.log("here")
 
-        x1.domain([d3.min(rawData, function(d) {
+        x1.domain([d3.min(rawData, function (d) {
             if (d.IndicatorName == new_option) {
                 let min_value = Infinity
-                for (let i = 1993; i <= 2021; i++) {
+                for (let i = 1993; i <= 2020; i++) {
                     if (d[i] != 0) {
                         console.log(d[i])
                         if (d[i] < min_value) {
@@ -188,23 +223,24 @@ d3.csv("data/india_dataset.csv").then(rawData =>{
                         }
                     }
                 }
-            return min_value
+                return min_value
             }
-        }), d3.max(rawData, function(d) {
+        }), d3.max(rawData, function (d) {
             if (d.IndicatorName == new_option) {
                 let max_value = 0
-                for (let i = 1993; i <= 2021; i++) {
+                for (let i = 1993; i <= 2020; i++) {
                     if (d[i] > max_value) {
                         max_value = d[i]
                     }
                 }
-            return max_value
+                return max_value
             }
         })])
         xAxisCall = d3.axisBottom(x1)
-        .ticks(7)
+            .ticks(7)
 
-        g1.selectAll(".x_axis").transition().duration(5000).call(xAxisCall)
+        //g1
+        svg.selectAll(".x_axis").transition().duration(5000).call(xAxisCall)
 
         let updatedFilteredData = rawData.filter(d => d.IndicatorName === new_option || d.IndicatorCode === "EG.ELC.ACCS.ZS")
 
@@ -213,14 +249,14 @@ d3.csv("data/india_dataset.csv").then(rawData =>{
         console.log(updatedFilteredData)
 
         if (updatedFilteredData[1]["IndicatorName"] == "GDP (current US$)") {
-            for (let i = 1993; i <= 2021; i ++) {
+            for (let i = 1993; i <= 2020; i++) {
                 if (updatedFilteredData[1][i] != 0) {
                     let updated1point = [updatedFilteredData[0][i], updatedFilteredData[1][i]]
                     updated1dataPoints.push(updated1point)
                 }
             }
         } else {
-            for (let i = 1993; i <= 2021; i ++) {
+            for (let i = 1993; i <= 2020; i++) {
                 if (updatedFilteredData[1][i] != 0) {
                     let updated1point = [updatedFilteredData[1][i], updatedFilteredData[0][i]]
                     updated1dataPoints.push(updated1point)
@@ -228,34 +264,128 @@ d3.csv("data/india_dataset.csv").then(rawData =>{
             }
         }
         console.log(updated1dataPoints)
-        
+
         graph_path.datum(updated1dataPoints)
-        .attr("d", d3.line()
-        .x(function(d) {
-            return x1(d[0])
-        }).y(function(d) {
-            return y1(d[1])
-        }))
-        .attr("stroke", "#138808")
+            .attr("d", d3.line()
+                .x(function (d) {
+                    return x1(d[0])
+                }).y(function (d) {
+                    return y1(d[1])
+                }))
+            .attr("stroke", "#138808")
 
         let length = graph_path.node().getTotalLength()
 
         graph_path.attr("stroke-dasharray", length + " " + length)
-        .attr("stroke-dashoffset", length)
-        .transition()
-        .duration(5000)
-        .attr("stroke-dashoffset", 0)
+            .attr("stroke-dashoffset", length)
+            .transition()
+            .duration(5000)
+            .attr("stroke-dashoffset", 0)
 
-        g1.selectAll(".x_label").remove()
+        //g1
+        svg.selectAll(".x_label").remove()
 
-        g1.append("text")
-        .classed("x_label", true)
-        .attr("x", width / 2)
-        .attr("y", height + 50)
-        .attr("font-size", "20px")
-        .attr("text-anchor", "middle")
-        .text(new_option)
+        //g1
+        svg.append("text")
+            .classed("x_label", true)
+            .attr("x", width / 2)
+            .attr("y", height + 40)
+            .attr("font-size", "20px")
+            .attr("text-anchor", "middle")
+            .text(optionLabels[new_option])
 
     })
-    }
-)
+
+
+})
+
+// parallel coordinates plot
+d3.csv("data/india_dataset_gdpindicators.csv").then(rawData => {
+
+
+    var margin = { top: 60, right: 10, bottom: 30, left: 10 },
+        width = screenWidth - margin.left - margin.right,
+        height = screenHeight / 2 - margin.top - margin.bottom;
+
+    var svg = d3.select("#parallel-coordinates").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    let data = rawData.map(d => {
+        return dimensions.reduce((acc, dimension) => {
+            acc[dimension] = +d[dimension];
+            return acc;
+        }, {});
+    });
+
+    var yScales = {};
+    dimensions.forEach(function (dimension) {
+        yScales[dimension] = d3.scaleLinear()
+            .domain(d3.extent(data, function (d) { return +d[dimension]; }))
+            .range([height, 0]);
+    });
+
+    // x axis 
+    var xScale = d3.scalePoint()
+        .range([0, width])
+        .padding(1)
+        .domain(dimensions);
+
+    svg.selectAll("myPath")
+        .data(data)
+        .enter()
+        .append("path")
+        .attr("d", function (d) {
+            return d3.line()(dimensions.map(function (p) { return [xScale(p), yScales[p](d[p])]; }));
+        })
+        .style("fill", "none")
+        .style("stroke", "steelblue")
+        .style("opacity", 0.5);
+
+    // !!!!!!!!!   https://d3-graph-gallery.com/graph/parallel_custom.html    !!!!!
+
+    // dimensions.forEach(function (d) {
+    //     svg.append("g")
+    //         .attr("transform", "translate(" + xScale(d) + ")")
+    //         .each(function (d) { d3.select(this).call(d3.axisLeft().scale(yScales[d])); })
+    //         .append("text")
+    //         .style("text-anchor", "middle")
+    //         .attr("y", -9)
+    //         .text(function (d) { return d; });
+    // });
+
+    svg.selectAll(".axis")
+        .data(dimensions).enter()
+        .append("g")
+        .attr("transform", function (d) { return "translate(" + xScale(d) + ")"; })
+        .each(function (d) {
+            d3.select(this).call(d3.axisLeft(yScales[d]));
+        })
+        .append("text")
+        .style("text-anchor", "middle")
+        .attr("y", height + margin.bottom - 10)
+        .text(function (d) { return dimensionLabels[d]; })
+        .style("font-weight", "bold")
+        .style("fill", "black")
+        .style("font-size", "14px");
+
+
+
+})
+
+// bar charts - select between the indicators
+d3.csv("data/india_dataset_gdpindicators.csv").then(rawData => {
+    var margin = { top: 30, right: 140, bottom: 60, left: 90 },
+        width = 550 - margin.left - margin.right,
+        height = 480 - margin.top - margin.bottom;
+
+    var svg = d3.select("#bar-chart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + (margin.left) + "," + margin.top + ")");
+    
+})
